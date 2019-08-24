c645c3a3d4d00d1b51d77566bf98aff198352c5a
saimekala <40240435+saimekala@users.noreply.github.com>
2019-08-23 21:04:58 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,42 @@
+package com.dixit.videodemo;
+
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+
+        videoView.start();
+
+/*
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+
+        MediaController mediaController = new MediaController(this);
+
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+
+        videoView.start();
+
+        */
+    }
+}
